53423c020abb0c3ff74a4aff7f6734e514791e3b
sonu <srssoumyaranjansahu123@gmail.com>
2023-07-29 08:57:45 +0530

    done with header calander and options home fetaures ..

---
+++ boooking/src/components/feature/Feature.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./feature.css"
+export const Feature = () => {
+  return (
+    <div>
+        <div className='feature'>
+            <div className='featureditm'>
+                
+    <img src='https://images.freeimages.com/images/large-previews/9d3/hotel-1217956.jpg' alt='ok' className='featuredimg'></img>
+              
+                <div className='feturetitles'>
+                <h1> ok</h1>
+                <h2> ok bro</h2>
+                </div>
+            </div>
+            <div className='featureditm'>
+                
+                <img src='https://images.freeimages.com/images/large-previews/b44/sylvia-hotel-1221419.jpg' alt='ok' className='featuredimg'></img>
+                          
+                            <div className='feturetitles'>
+                            <h1> ok</h1>
+                            <h2> ok bro</h2>
+                            </div>
+                        </div>
+                        <div className='featureditm'>
+                
+                <img src='https://images.freeimages.com/images/large-previews/d46/santorini-fira-2-1572367.jpg' alt='ok' className='featuredimg'></img>
+                          
+                            <div className='feturetitles'>
+                            <h1> ok</h1>
+                            <h2> ok bro</h2>
+                            </div>
+                        </div>
+        </div>
+    </div>
+  )
+}
--- boooking/src/pages/list/List.jsx
+++ boooking/src/pages/list/List.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import './list.css'
+import { Navbar } from '../../components/navbar/Navbar.jsx'
+import { Header } from '../../components/header/Header.jsx'
 
 export const List = () => {
   return (
-    <div>List</div>
+    <div><Navbar/>
+    <Header typeo="list"/></div>
   )
 }
